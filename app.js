45d14b56171a8037bd783e0002388e74c62981ad
Naveen Busireddy <84928714+naveenbusireddy@users.noreply.github.com>
2021-10-11 09:33:56 +0530

    JS file update

--- app.js
+++ app.js
@@ -1,20 +1,47 @@
 const inputA = document.querySelector("#inputA");
 const inputB = document.querySelector("#inputB");
-const add = document.querySelector("#addition");
+const addition = document.querySelector("#addition");
 const sub = document.querySelector("#substraction");
 const mul = document.querySelector("#multiplication");
 const div = document.querySelector("#division");
+const showMessage = document.querySelector("#output");
 
-var A = inputA.value;
-var B = inputB.value;
-
-function addition (){
-    console.log(A);
-    console.log(B);
+// function additionAB (){
+//     var A = inputA.value;
+//     var B = inputB.value;
     
-    var addAB = A+B;
-    console.log(addAB);  
-}
-// console.log(result);
+//     const additionOfAB = Number(A)+Number(B);    
+//     showMessage.innerText = 'sum of two numbers is: '+additionOfAB;   
+// }
 
-add.addEventListener("click", addition);
+addition.addEventListener("click", () => {
+    var A = inputA.value;
+    var B = inputB.value;
+
+    const additionOfAB = Number(A)+Number(B);    
+    showMessage.innerText = 'sum of two numbers is: '+additionOfAB;
+});
+
+sub.addEventListener("click", () => {
+    var A = inputA.value;
+    var B = inputB.value;
+
+    const substractionOfAB = Number(A)-Number(B);    
+    showMessage.innerText = 'substraction of two numbers is: '+substractionOfAB;
+});
+
+mul.addEventListener("click", () => {
+    var A = inputA.value;
+    var B = inputB.value;
+
+    const multiplicationOfAB = Number(A)*Number(B);    
+    showMessage.innerText = 'Multiplication of two numbers is: '+multiplicationOfAB;
+});
+
+div.addEventListener("click", () => {
+    var A = inputA.value;
+    var B = inputB.value;
+
+    const divisionOfAB = Number(A)/Number(B);    
+    showMessage.innerText = 'Division of two numbers is: '+divisionOfAB;
+});
