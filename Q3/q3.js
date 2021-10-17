84b75beaffae3187b4a2e753d11ddce918a339b8
Naveen Busireddy <84928714+naveenbusireddy@users.noreply.github.com>
2021-10-17 23:22:47 +0530

    question3

--- Q3/q3.js
+++ Q3/q3.js
@@ -1,30 +1,32 @@
-var text = document.querySelector("#q3-input");
+var input = document.getElementById("q3-input");
 var btn1 = document.querySelector("#btn1");
 var btn2 = document.querySelector("#btn2");
 var btn3 = document.querySelector("#btn3");
 var output = document.querySelector("#output");
+var output1 = document.querySelector("#output1");
+var output2 = document.querySelector("#output2");
 
-text.addEventListener("input", () => {
-    output.innerText = text.value;
-})
+// text.addEventListener("input", () => {
+//     output.innerText = text.value;
+// })
 
 btn1.addEventListener("click", () => {
 
     var h1 = document.createElement("h1");
     output.parentNode.replaceChild(h1, output);
-    h1.innerHTML = output.innerHTML;
-    // document.createElement("h1").innerHTML = text.innerHTML;
+    h1.innerHTML = input.innerHTML;
+    
 });
 
-// btn2.addEventListener("click", () => {
+btn2.addEventListener("click", () => {
 
-//     var h2 = document.createElement("h2");
-//     text.parentNode.replaceChild(h2, text);
-//     h2.innerHTML = text.innerHTML;
-// });
+    var h2 = document.createElement("h2");
+    output1.parentNode.replaceChild(h2, output1);
+    h2.innerHTML = input.innerHTML;
+});
 
 btn3.addEventListener("click", () => {
     var h3 = document.createElement("h3");
-    output.parentElement.appendChild(h3, output);
-    h3.innerHTML = output.innerHTML;
+    output2.parentNode.replaceChild(h3, output2);
+    h3.innerHTML = input.innerHTML;
 });
