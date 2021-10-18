f0652db4badc265b8fde48e17cd435a51086bccb
Naveen Busireddy <84928714+naveenbusireddy@users.noreply.github.com>
2021-10-18 12:07:30 +0530

    fix

--- Q3/q3.js
+++ Q3/q3.js
@@ -3,17 +3,14 @@ var btn1 = document.querySelector("#btn1");
 var btn2 = document.querySelector("#btn2");
 var btn3 = document.querySelector("#btn3");
 var output = document.querySelector("#output");
-var output1 = document.querySelector("#output1");
-var output2 = document.querySelector("#output2");
 
 // text.addEventListener("input", () => {
 //     output.innerText = text.value;
 // })
 
 btn1.addEventListener("click", () => {
-
     var h1 = document.createElement("h1");
-    output.parentNode.replaceChild(h1, output);
+    output.parentNode.append(h1, output);
     h1.innerHTML = input.innerHTML;
     
 });
@@ -21,12 +18,12 @@ btn1.addEventListener("click", () => {
 btn2.addEventListener("click", () => {
 
     var h2 = document.createElement("h2");
-    output1.parentNode.replaceChild(h2, output1);
+    output.parentNode.append(h2, output);
     h2.innerHTML = input.innerHTML;
 });
 
 btn3.addEventListener("click", () => {
     var h3 = document.createElement("h3");
-    output2.parentNode.replaceChild(h3, output2);
+    output.parentNode.append(h3, output);
     h3.innerHTML = input.innerHTML;
 });
