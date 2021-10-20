c53b3cb8bc933db4fadd734bdfb3832e4ff891fb
Naveen Busireddy <84928714+naveenbusireddy@users.noreply.github.com>
2021-10-20 14:10:41 +0530

    fix

--- Q11/q11.js
+++ Q11/q11.js
@@ -1,20 +1,33 @@
 const input = document.getElementById("password");
 const btn = document.getElementById("button");
+const output = document.querySelector("#output");
 
 btn.disabled = true;
 
 input.addEventListener("input", () => {
-    // let inputLength = input.value
-    if(input.value.length < 8) 
+    let value = input.value;
+    if(value.length < 8)
+    {
+        btn.disabled = true;
+        input.style.backgroundColor = "gray";
+    } 
+    else 
     {
         btn.disabled = false;
-    } else {
-        btn.disabled = true;
+        input.style.backgroundColor = "lightgreen";
+
     }
 })
 
+btn.addEventListener("click", () => {
+    let value = input.value;
+    if(value.length < 10) {
+        output.innerText = "Error";
+        output.style.color = "red";
+    } else {
+        output.innerText = "Success";
+        output.style.color = "green";
 
-// btn.addEventListener("click", () => {
-    
-//     console.log("clicked");
-// })
+    }
+})
+
