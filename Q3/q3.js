548b6bbc739ebbd855e8fb810a74e161bbba9d78
Naveen Busireddy <84928714+naveenbusireddy@users.noreply.github.com>
2021-10-21 13:07:33 +0530

    Update q3.js

--- Q3/q3.js
+++ Q3/q3.js
@@ -9,21 +9,24 @@ var output = document.querySelector("#output");
 // })
 
 btn1.addEventListener("click", () => {
-    var h1 = document.createElement("h1");
-    output.parentNode.append(h1, output);
-    h1.innerHTML = input.innerHTML;
-    // output.replaceWith(h1);
+    input.innerHTML = `<h1>${input.innerText}</h1>`;
+    /* var h1 = document.createElement("h1");
+    output.parentNode.append(h1, output);    
+    output.replaceWith(h1); */
 });
 
 btn2.addEventListener("click", () => {
+    input.innerHTML = `<h2>${input.innerText}</h2>`;
 
-    var h2 = document.createElement("h2");
+    /* var h2 = document.createElement("h2");
     output.parentNode.append(h2, output);
-    h2.innerHTML = input.innerHTML;
+    h2.innerHTML = input.innerHTML; */
 });
 
 btn3.addEventListener("click", () => {
-    var h3 = document.createElement("h3");
+    input.innerHTML = `<h3>${input.innerText}</h3>`;
+
+    /* var h3 = document.createElement("h3");    
     output.parentNode.append(h3, output);
-    h3.innerHTML = input.innerHTML;
+    h3.innerHTML = input.innerHTML; */
 });
