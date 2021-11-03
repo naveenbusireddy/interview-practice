46740856826ea9cd41d16efa566a402bbdf14997
Naveen Busireddy <84928714+naveenbusireddy@users.noreply.github.com>
2021-11-03 19:03:03 +0530

    fix

--- Q7/q7.js
+++ Q7/q7.js
@@ -1,13 +1,23 @@
 var input = document.querySelector("#input-data");
 var btn = document.querySelector("#submit");
+var output = document.querySelector("#output");
 
 const URL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+
 function fetchURL() {
-    // console.log("hello");
+    // console.log("hello");  
     var text = input.value;
-    var newURL = URL+"/?" +text;
+    var newURL = URL+ "?text=" +text;
+     
     fetch(newURL)
-    .then(Response => Response.text())
-    .then(data => console.log(data));
+    .then(Response => Response.json())
+    // .then(data => output.innerHTML = data)
+
+    .then(data => {
+        var translatedText = data.contents.translated;
+        output.innerHTML = translatedText;
+    } )
+
+    // console.log("hello");
 
 }
